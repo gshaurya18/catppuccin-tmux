3b4977354712dd529a5f396c532da05cfa3e214c
Mark Pitblado <mark@pitblado.me>
2024-11-22 22:10:24 -0800

    init commit of fork

---
+++ README.md
@@ -0,0 +1,6 @@
+# Catppuccin Tmux
+
+This repository is a fork of [tokyo-night-tmux](https://github.com/janoamaral/tokyo-night-tmux) by Logico. The license of that code was the MIT license, and is maintained in this repository through the `LICENSE.md` file. A huge thank you to their development efforts, whichout which this would not be possible.
+
+The primary purpose of this repository is to take the visual outline of the tokyo-night-tmux plugin but change the colours and themes to be catppuccin. There is also the [tmux repository in the catppuccin organization](https://github.com/catppuccin/tmux) which you may also experiment with and decide on your preference.
+
---
+++ src/themes.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+SELECTED_THEME="$(tmux show-option -gv @tokyo-night-tmux_theme)"
+
+case $SELECTED_THEME in
+"latte")
+  declare -A THEME=(
+    ["background"]="#24283b" # Base
+    ["foreground"]="#8c8fa1" # Overlay 2
+    ["black"]="#4c4f69" # Text
+    ["blue"]="#1e66f5" # Blue
+    ["cyan"]="#04a5e5" # Sky
+    ["green"]="#40a02b" # Green
+    ["magenta"]="#ea76cb" # Pink
+    ["red"]="#d20f39" # Red
+    ["white"]="#dce0e8" # Crust
+    ["yellow"]="#df8e1d" # Yellow
+
+    ["bblack"]="#414868"
+    ["bblue"]="#7aa2f7"
+    ["bcyan"]="#7dcfff"
+    ["bgreen"]="#41a6b5"
+    ["bmagenta"]="#bb9af7"
+    ["bred"]="#f7768e"
+    ["bwhite"]="#787c99"
+    ["byellow"]="#e0af68"
+  )
+  ;;
+
+"frappe")
+  declare -A THEME=(
+    ["background"]="#303446" # Base
+    ["foreground"]="#949cbb" # Overlay 2
+    ["black"]="#c6d0f5" # Text
+    ["blue"]="#8caaee" # Blue
+    ["cyan"]="#99d1db" # Sky
+    ["green"]="#a6d189" # Green
+    ["magenta"]="#f4b8e4" # Pink
+    ["red"]="#e78284" # Red
+    ["white"]="#232634" # Crust
+    ["yellow"]="#e5c890" # Yellow
+
+    ["bblack"]="#9699a3"
+    ["bblue"]="#34548a"
+    ["bcyan"]="#0f4b6e"
+    ["bgreen"]="#33635c"
+    ["bmagenta"]="#5a4a78"
+    ["bred"]="#8c4351"
+    ["bwhite"]="#343b58"
+    ["byellow"]="#8f5815"
+  )
+  ;;
+
+
+"Macchiato")
+  declare -A THEME=(
+    ["background"]="#24273a" # Base
+    ["foreground"]="#939ab7" # Overlay 2
+    ["black"]="#cad3f5" # Text
+    ["blue"]="#8aadf4" # Blue
+    ["cyan"]="#91d7e3" # Sky
+    ["green"]="#a6da95" # Green
+    ["magenta"]="#f5bde6" # Pink
+    ["red"]="#ed8796" # Red
+    ["white"]="#181926" # Crust
+    ["yellow"]="#eed49f" # Yellow
+
+    ["bblack"]="#9699a3"
+    ["bblue"]="#34548a"
+    ["bcyan"]="#0f4b6e"
+    ["bgreen"]="#33635c"
+    ["bmagenta"]="#5a4a78"
+    ["bred"]="#8c4351"
+    ["bwhite"]="#343b58"
+    ["byellow"]="#8f5815"
+  )
+  ;;
+
+*)
+  # Default to mocha theme
+  declare -A THEME=(
+    ["background"]="#1e1e2e" # Base
+    ["foreground"]="#9399b2" # Overlay 2
+    ["black"]="#cdd6f4" # Text
+    ["blue"]="#89b4fa" # Blue
+    ["cyan"]="#89dceb" # Sky
+    ["green"]="#a6e3a1"
+    ["magenta"]="#f5c2e7"
+    ["red"]="#f38ba8"
+    ["white"]="#11111b"
+    ["yellow"]="#f9e2af"
+
+    ["bblack"]="#2A2F41"
+    ["bblue"]="#7aa2f7"
+    ["bcyan"]="#7dcfff"
+    ["bgreen"]="#41a6b5"
+    ["bmagenta"]="#bb9af7"
+    ["bred"]="#ff9e64"
+    ["bwhite"]="#787c99"
+    ["byellow"]="#e0af68"
+  )
+  ;;
+esac
+
+THEME['ghgreen']="#a6e3a1" # Mocha Green
+THEME['ghmagenta']="#eba0ac" # Mocha Maroon
+THEME['ghred']="#f38ba8" # Mocha Red
+THEME['ghyellow']="#f9e2af" # Mocha Yellow
+
+RESET="#[fg=${THEME[foreground]},bg=${THEME[background]},nobold,noitalics,nounderscore,nodim]"
